fe8cb40b3eefe5329e7f625d099d1a30346df31f
Aarchie-05 <aarchie.girdhar10@gmail.com>
2022-06-22 13:52:30 +0530

    home urls added

--- SmartComp/urls.py
+++ SmartComp/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('compare.urls')),
+    path('', include('home.urls')),
+    path('compare/', include('compare.urls')),
     path('admin/', admin.site.urls),
 ]
--- compare/urls.py
+++ compare/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.compare, name='compare'),
+    path('', views.compare, name='compare1'),
     path('search', views.search, name='search'),
     path('data', views.AjaxHandlerView.as_view())
 ]
