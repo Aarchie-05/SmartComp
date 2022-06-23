7771d5a7e9ec545ca4c7164ebd314a92fcf20132
paras-ch <paras1638.cse19@chitkara.edu.in>
2022-06-23 05:30:51 +0530

    Snapdeal Data Recieved

--- home/views.py
+++ home/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
 from django.views.generic import View
 from .tasks import *
 
@@ -20,17 +18,8 @@ class AjaxHandlerView(View):
                 pass
             elif store == 'amazon':
                 data = amazon_best_deals.delay()
-                return JsonResponse({'data': data.get()}, status=200)
             elif store == 'snapdeal':
-                pass
+                data = snapdeal_best_deals.delay()
+
+        return JsonResponse({'data': data.get()}, status=200)
           
-
-
-
-
-
-
-
-
-
-
