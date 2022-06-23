37f039e57e6d4c1b191e58d15d1cd172f83d19f7
Aarchie-05 <aarchie.girdhar10@gmail.com>
2022-06-23 05:31:40 +0530

    data retrieved

--- static/js/script.js
+++ static/js/script.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
   $.ajax({
     url: 'data',
     type: 'get',
-    data: { search: 'shampoo', store: 'amazon', req_data: 'primary'}
+    data: { search: 'shampoo', store: 'snapdeal', req_data: 'other'}
   })
   .done((res) => {
     console.log(res)
