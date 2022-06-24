6ba764dc9dc5e783437301c5eeaa3154c5eccdff
Aarchie-05 <aarchie.girdhar10@gmail.com>
2022-06-24 22:26:49 +0530

    urls amendment

--- compare/urls.py
+++ compare/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.compare, name='compare1'),
-    path('search', views.search, name='search'),
     path('data', views.AjaxHandlerView.as_view())
 ]
